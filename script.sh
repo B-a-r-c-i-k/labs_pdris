cbc1acfec38de2a15d51126c2a9250b094b47458
Shirokov <kir100803@gmail.com>
2024-10-25 22:22:30 +0300

    fix csv file

--- script.sh
+++ script.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 monitoring_params() {
-	for ((;;))
+	timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+	date_only=$(date +"%Y-%m-%d")
+
+	filename="monitoring_${date_only}_${timestamp}.csv"
+
+	while true
 	do
-		df -h | grep /dev/sda2 | awk '{print $4}' >> a.txt
+		current_date_only=$(date +"%Y-%m-%d")
+
+		if [[ ! -f "monitoring_${current_date_only}_${timestamp}.csv" ]] 
+		then
+			date_only="$current_date_only"
+			timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+			filename="monitoring_${date_only}_${timestamp}.csv"
+			echo "Time,Disk Usage" > "$filename"
+		fi
+		disk_usage=$(df -h | grep /dev/sda2 | awk '{print $4}')
+		echo "$(date +"%H:%M:%S"),$disk_usage" >> "$filename"
 		sleep 10
 	done
 }
